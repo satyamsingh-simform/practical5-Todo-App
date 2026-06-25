e5d3f95293df77b07c772db623dd5731b00d9e51
Satyam <satyam.singh@simformsolutions.com>
2026-06-25 12:54:13 +0530

    fix:theme issue

--- src/utils/useTheme.tsx
+++ src/utils/useTheme.tsx
@@ -4,8 +4,13 @@ export default function useTheme(){
     const [darkMode,setDarkMode]=useState(false);
 
     useEffect(()=>{
-        document.body.classList.toggle('body-bg')
-    },[darkMode])
+        if(darkMode){
+            document.body.classList.add("body-bg");
+        } 
+        else {
+            document.body.classList.remove("body-bg");
+        }
+    }, [darkMode]);
 
     function handleModeToggle(){
         setDarkMode(prev=>!prev)
